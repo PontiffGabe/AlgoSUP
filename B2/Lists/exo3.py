a4658d94e5cdc10846324ffa35d2b7d866d6730b
PontiffGabe <129831757+PontiffGabe@users.noreply.github.com>
2023-12-29 23:05:49 +0100

    Update exo3.py

--- B2/Lists/exo3.py
+++ B2/Lists/exo3.py
@@ -17,7 +17,7 @@ def is_sorted(l:list) -> bool:
     return sorted
 
 """
-exo 3.3
+exo 3.3, selection sort
 """
 
 def minimum(l: list, d:int, f:int) -> int:
@@ -43,5 +43,40 @@ print(select_sort([-3,-2,-1,0,1,2,3]))
 
 
 """
-exo 3.4
+exo 3.4, insertion sort
 """
+
+def insert_sort(l:list):
+    for i in range(len(l)):
+        j = i
+        while j>0 and l[j] < l[j-1]:
+            l[j], l[j-1] = l[j-1], l[j]
+            j-=1
+    return l
+
+a = [3,6,1,9,2,2, 9]
+insert_sort(a)
+print(a)
+
+"""
+exo 4.1, binary search
+"""
+
+def binary_search(l: list, x:int) -> int:
+
+    left = 0
+    right = len(l)-1
+
+    while left <= right:
+        middle = (left+right)//2
+        if l[middle] == x:
+            return middle
+        elif l[middle] < x:
+            left = middle + 1
+        else:
+            right = middle -1
+    return -1
+
+
+
+
