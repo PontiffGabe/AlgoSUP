0d8a758ad37aac30fc7569427e1adf5ffe77a532
PontiffGabe <129831757+PontiffGabe@users.noreply.github.com>
2024-03-06 18:50:55 +0100

    Building

--- SUP/B3/ArbresBinaires/exo1.py
+++ SUP/B3/ArbresBinaires/exo1.py
@@ -1,3 +1,39 @@
-from ArbreBinaire import *
+from ArbreBinaire import Noeud
+
+"""
+Exo 1.1: Taille (Size)
+
+Axiome : taille(arbre_vide) = 0
+[taille(noeud) = 1 + taille(sous_arbre_gauche) + taille(sous_arbre_droite)]
+"""
+
+def taille(abr : Noeud) -> int:
+    if abr == None:
+        return 0
+    else:
+        return 1 + taille(abr.sag) + taille(abr.sad)
+    
 
 
+"""
+Exo 1.2: Hauteur (Height)
+"""
+
+def hauteur(abr: Noeud) -> int:
+    if abr == None:
+        return -1
+    else:
+        return 1 + max(hauteur(abr.sag), hauteur(abr.sad))
+    
+
+n4 = Noeud(9)
+n5 = Noeud(6)
+n7 = Noeud(9)
+
+n3 = Noeud(2, None, n7)
+n2 = Noeud(4, n4, n5)
+
+abr = Noeud(1, n2, n3)
+
+print(taille(abr))
+print(hauteur(abr))
---
+++ SUP/B3/ArbresBinaires/exo2.py
@@ -0,0 +1,26 @@
+from ArbreBinaire import Noeud
+"""
+Exo 2.1 DFS : Parcours profondeur (Depth-First Search)
+"""
+
+def afficher_arbre(abr: Noeud) -> str:
+    if abr == None:
+        return '_'
+    else:
+        return f"<{abr.valeur}, {afficher_arbre(abr.sag)}, {afficher_arbre(abr.sad)}>"
+
+
+
+
+
+
+n4 = Noeud(9)
+n5 = Noeud(6)
+n7 = Noeud(9)
+
+n3 = Noeud(2, None, n7)
+n2 = Noeud(4, n4, n5)
+
+abr = Noeud(1, n2, n3)
+
+print(afficher_arbre(abr))
